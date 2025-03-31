572e8aa387627ecb6b79637fb2da2c2d9a0fa3a6
oliwka222 <oliwiaj456@gmail.com>
2025-03-31 15:56:01 +0200

    General fix

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'habit_tracker_db',
-  password: process.env.DB_PASSWORD || 'thisisagoodpassword',
-  port: process.env.DB_PORT || 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
   ssl : false,
 });
 
