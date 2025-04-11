54cb8ee1407ab5993625306b53e4499f0a99f6b2
oliwka222 <oliwiaj456@gmail.com>
2025-04-11 15:44:50 +0200

    Connecting to frontend

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
 app.use(express.json());
 
 const authRoutes = require('./routes/auth');
