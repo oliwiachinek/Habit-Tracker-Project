41337969d372d9412fab525efd31f529db22f395
Chidoo22 <chidonicole4@gmail.com>
2025-05-09 16:37:26 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,11 @@
-const pool = require('../config/db');
+const pool = require('../db');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const registerUser = async (firstName, lastName, email, password) => {
   const existingUser = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
   console.log(existingUser.rows);
-  if (existingUser.rows.lenghtb > 0) {
+  if (existingUser.rows.length > 0) {
     throw new Error('Email already in use');
   }
 
@@ -14,15 +14,13 @@ const registerUser = async (firstName, lastName, email, password) => {
     'INSERT INTO users (first_name, last_name, email, password_hash) VALUES ($1, $2, $3, $4) RETURNING *',
     [firstName, lastName, email, hashedPassword]
   );
-  const createProfile = async (user_id, full_name, email, avatar) => {
-    const result = await pool.query(
-        'INSERT INTO profiles (user_id, full_name, email, avatar) VALUES ($1, $2, $3, $4) RETURNING *',
-        [user_id, full_name, email, avatar]
-    );
-    return result.rows[0];
-};
-
   const user = result.rows[0];
+
+  await pool.query(
+    'INSERT INTO profiles (user_id, full_name, email, avatar) VALUES ($1, $2, $3, $4)',
+    [user.user_id, `${user.first_name} ${user.last_name}`, user.email, 'default-avatar.png']
+  );
+
   const token = jwt.sign({ id: user.user_id }, process.env.JWT_SECRET, {
     expiresIn: '1h',
   });
@@ -37,10 +35,12 @@ const loginUser = async (email, password) => {
   const isValid = await bcrypt.compare(password, user.rows[0].password_hash);
   if (!isValid) throw new Error('Invalid password');
 
-  const token = jwt.sign({ id: user.rows[0].user_id }, process.env.JWT_SECRET, {
+  const token = jwt.sign({ id: user.rows[0].id }, process.env.JWT_SECRET, {
     expiresIn: '1h',
   });
+
   return token;
 };
+
 
 module.exports = { registerUser, loginUser };
